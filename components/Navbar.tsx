d40c3733bbdbef51d5b6ccd59c04e2d254bb512a
adarshnaidu4@gmail.com <adarshnaidu4@gmail.com>
2023-09-16 19:40:48 +0530

    navbar and navlinks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
-const Navbar = () => {
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "../public/logo.png";
+import Button from "./Button";
+import { NavLinks } from "@/constant/Index";
+const navbar = async () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src={logo} width={116} height={43} alt="logo" />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.text}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default navbar;
